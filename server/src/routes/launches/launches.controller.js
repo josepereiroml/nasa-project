afcacd955bc333e46e9fbf6ef0ea2ffdc7a9314f
Jose Pereiro <65933374+josepereiroml@users.noreply.github.com>
2021-10-10 23:50:47 -0500

    Add DELETE http and tests

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
+const {
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
+} = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
@@ -28,7 +33,29 @@ function httpAddNewLaunch(req, res) {
 
   return res.status(201).json(launch);
 }
+async function httpAbortLaunch(req, res) {
+  const launchId = Number(req.params.id);
+
+  const existsLaunch = await existsLaunchWithId(launchId);
+  if (!existsLaunch) {
+    return res.status(404).json({
+      error: "Launch not found",
+    });
+  }
+
+  const aborted = await abortLaunchById(launchId);
+  if (!aborted) {
+    return res.status(400).json({
+      error: "Launch not aborted",
+    });
+  }
+
+  return res.status(200).json({
+    ok: true,
+  });
+}
 module.exports = {
   httpGetAllLaunches,
   httpAddNewLaunch,
+  httpAbortLaunch,
 };
